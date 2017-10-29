d81078ab4e1082306e0c8087d2781d72e503c726
camzillajs101 <camilomason0@gmail.com>
2017-10-29 19:01:10 -0400

    Renaming table and rake db:migrate

--- groceries/db/migrate/20171029225704_create_groceries.rb
+++ groceries/db/migrate/20171029225704_create_groceries.rb
@@ -1,6 +1,8 @@
 class CreateGroceries < ActiveRecord::Migration[5.0]
   def change
-    create_table :groceries do |t|
+    create_table :grocery do |t|
+      t.string :name
+      t.integer :quantity
     end
   end
 end
