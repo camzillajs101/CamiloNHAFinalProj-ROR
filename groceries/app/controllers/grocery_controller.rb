293431be6de01370e7a4e95a8cbd5cbab9dc44b4
camzillajs101 <camilomason0@gmail.com>
2017-10-30 20:20:23 -0400

    Adding POST actions

--- groceries/app/controllers/grocery_controller.rb
+++ groceries/app/controllers/grocery_controller.rb
@@ -12,4 +12,23 @@ class GroceryController < ApplicationController
   def show
     @item = Grocery.find(params[:id])
   end
+
+  # POST actions
+  def create
+    params.permit!
+    @item = Grocery.new(params[:grocery])
+    if @item.save
+      render action: :show
+    else
+      render action: :new
+    end
+  end
+  def destroy_all
+    Grocery.destroy_all
+    redirect_to action: :index
+  end
+  def destroy
+    Grocery.where(id: params[:id]).destroy_all
+    redirect_to action: :index
+  end
 end
